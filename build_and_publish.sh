75ac50d8763cc80a28582fc89c3ffd6155ba9b40
Ivar Nesje <ivar.nesje@bouvet.no>
2019-08-08 14:20:31 +0200

    Oppdater docker script i build_and_publish.sh

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -1,5 +1,10 @@
+# Docker notater
+
+# I hovedsak er det følgende komando som skal kjøres
+docker build -t bouvet-ctf . && docker run -it --rm -p 5000:80 --name bouvet-ctf_sample bouvet-ctf
+
+# For deploy trengs også følgende
 #docker login bouvetctf.azurecr.io
-#docker run -it --rm -p 5000:80 --name bouvet-ctf_sample bouvet-ctf
-docker build -t bouvet-ctf .
-docker tag bouvet-ctf bouvetctf.azurecr.io/bouvet-ctf
-docker push bouvetctf.azurecr.io/bouvet-ctf
+#docker build -t bouvet-ctf .
+#docker tag bouvet-ctf bouvetctf.azurecr.io/bouvet-ctf
+#docker push bouvetctf.azurecr.io/bouvet-ctf
