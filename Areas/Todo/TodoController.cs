c344d65dfcc2bf426ea913fd47aadb95ae01e345
Ivar Nesje <ivar.nesje@bouvet.no>
2019-08-12 10:46:24 +0200

    Fixes for improved nulluable types

--- Areas/Todo/TodoController.cs
+++ Areas/Todo/TodoController.cs
@@ -8,33 +8,34 @@ using Microsoft.AspNetCore.Authorization;
 using Ctf.Models;
 using Ctf.Repositories;
 
-namespace Ctf.Areas.Todo{
-    [Area("Todo")]
-    public class TodoController : Controller
-    {
-        public TodoController(TodoRepository todoRepository)
-        {
-            _todoRepository = todoRepository;
-        }
-        private readonly TodoRepository _todoRepository;
-        public async Task<IActionResult> Index()
-        {
-            return View(new TodoViewModel( await _todoRepository.GetTodos(User.Identity.IsAuthenticated ? User.Identity.Name : "Public")));
-        }
-        [Authorize]
-        [HttpPost]
-        public async Task<IActionResult> Todo(TodoDto todo)
-        {
-            if(ModelState.IsValid)
-            {
-                await _todoRepository.CreateTodo(new Todo(0, todo.Description??"", User.Identity.Name));
-                return RedirectToAction("Index");
-            }
-            return  RedirectToAction("Index");
-        }
-        public async Task<IActionResult> Show(int id)
-        {
-            return View( await _todoRepository.GetTodo(id));
-        }
-    }
+namespace Ctf.Areas.Todo
+{
+	[Area("Todo")]
+	public class TodoController : Controller
+	{
+		public TodoController(TodoRepository todoRepository)
+		{
+			_todoRepository = todoRepository;
+		}
+		private readonly TodoRepository _todoRepository;
+		public async Task<IActionResult> Index()
+		{
+			return View(new TodoViewModel(await _todoRepository.GetTodos((User.Identity.IsAuthenticated ? User.Identity.Name : null) ?? "Public")));
+		}
+		[Authorize]
+		[HttpPost]
+		public async Task<IActionResult> Todo(TodoDto todo)
+		{
+			if (ModelState.IsValid)
+			{
+				await _todoRepository.CreateTodo(new Todo(0, todo.Description ?? "", User.Identity.Name!));
+				return RedirectToAction("Index");
+			}
+			return RedirectToAction("Index");
+		}
+		public async Task<IActionResult> Show(int id)
+		{
+			return View(await _todoRepository.GetTodo(id));
+		}
+	}
 }
